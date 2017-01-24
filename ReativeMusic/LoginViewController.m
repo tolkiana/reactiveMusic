07eb494573b7ff74a98a1def181fea1cc7d7b197
nelidaVelazquez <nelida.velazquez@dominos.com>
2017-01-24 14:16:50 -0500

    Create methods to check authentication status and to renew token

--- ReativeMusic/LoginViewController.m
+++ ReativeMusic/LoginViewController.m
@@ -7,11 +7,46 @@
 //
 
 #import "LoginViewController.h"
+#import <SpotifyAuthentication/SpotifyAuthentication.h>
+#import <SafariServices/SafariServices.h>
+
 
 @interface LoginViewController ()
 
 @end
 
 @implementation LoginViewController
+
+- (void)checkAuthenticationStatus {
+    SPTAuth *auth = [SPTAuth defaultInstance];
+    
+    // Check if we have a token at all
+    if (auth.session == nil) {
+        return;
+    }
+    // Check if it's still valid
+    if ([auth.session isValid]) {
+        // It's still valid, show the player.
+        // Perform segue to SearchViewController
+        return;
+    }
+    // Oh noes, the token has expired, if we have a token refresh service set up, we'll call tat one.
+    if (auth.hasTokenRefreshService) {
+        [self renewToken];
+        return;
+    }
+}
+
+- (void)renewToken {
+    SPTAuth *auth = [SPTAuth defaultInstance];
+    [auth renewSession:auth.session callback:^(NSError *error, SPTSession *session) {
+        auth.session = session;
+        if (error) {
+            NSLog(@"*** Error renewing session: %@", error);
+            return;
+        }
+    }];
+}
+
 
 @end
