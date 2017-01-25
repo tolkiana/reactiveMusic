9cf46767ad9ce1ce54c4c941c6847f4e3672b7e8
nelidaVelazquez <nelida.velazquez@dominos.com>
2017-01-24 19:46:18 -0500

    Removes commented out code and adds some pragmas

--- ReativeMusic/SearchViewController.m
+++ ReativeMusic/SearchViewController.m
@@ -46,16 +46,7 @@ static NSString * const kTrackCellIdentifier = @"TrackCellIdentifier";
     return cell;
 }
 
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
+#pragma mark - Search Methods
 
 - (void)searchWithString:(NSString *)string {
     
