37f1df82d65bd71e5292dffb2fda07b1305e3f8f
nelidaVelazquez <nelida.velazquez@dominos.com>
2017-01-24 21:22:24 -0500

    Creates extension to get a color with an hexadecimal value

---
+++ ReativeMusic/UIColor+Utilities.h
@@ -0,0 +1,15 @@
+//
+//  UIColor+Utilities.h
+//  ReativeMusic
+//
+//  Created by Nelida Velazquez on 1/24/17.
+//  Copyright © 2017 tolkiana. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIColor (Utilities)
+
++ (UIColor *)colorWithHexString:(NSString *)hexString;
+
+@end
---
+++ ReativeMusic/UIColor+Utilities.m
@@ -0,0 +1,22 @@
+//
+//  UIColor+Utilities.m
+//  ReativeMusic
+//
+//  Created by Nelida Velazquez on 1/24/17.
+//  Copyright © 2017 tolkiana. All rights reserved.
+//
+
+#import "UIColor+Utilities.h"
+
+@implementation UIColor (Utilities)
+
++ (UIColor *)colorWithHexString:(NSString *)hexString {
+    unsigned rgbValue = 0;
+    NSScanner *scanner = [NSScanner scannerWithString:hexString];
+    [scanner setScanLocation:1]; // bypass '#' character
+    [scanner scanHexInt:&rgbValue];
+    
+    return [UIColor colorWithRed:((rgbValue & 0xFF0000) >> 16)/255.0 green:((rgbValue & 0xFF00) >> 8)/255.0 blue:(rgbValue & 0xFF)/255.0 alpha:1.0];
+}
+
+@end
