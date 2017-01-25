f43b79128a2703a32c5e5ac8059d0ed8f8ca90c2
nelidaVelazquez <nelida.velazquez@dominos.com>
2017-01-24 19:16:29 -0500

    Creates TrackTableViewCell

---
+++ ReativeMusic/TrackTableViewCell.h
@@ -0,0 +1,16 @@
+//
+//  TrackTableViewCell.h
+//  ReativeMusic
+//
+//  Created by Nelida Velazquez on 1/24/17.
+//  Copyright © 2017 tolkiana. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TrackTableViewCell : UITableViewCell
+
+@property (strong, nonatomic) IBOutlet UILabel *trackTitle;
+@property (strong, nonatomic) IBOutlet UILabel *artistName;
+
+@end
---
+++ ReativeMusic/TrackTableViewCell.m
@@ -0,0 +1,15 @@
+//
+//  TrackTableViewCell.m
+//  ReativeMusic
+//
+//  Created by Nelida Velazquez on 1/24/17.
+//  Copyright © 2017 tolkiana. All rights reserved.
+//
+
+#import "TrackTableViewCell.h"
+
+@implementation TrackTableViewCell
+
+
+
+@end
