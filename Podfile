264d2ecf26affd7a89dda4db0c20bcd7a6c38373
nelidaVelazquez <nelida.velazquez@dominos.com>
2017-01-26 23:05:57 -0500

    Install Reactivecocoa with cocoapods intead of Carthage :(

---
+++ Podfile
@@ -0,0 +1,3 @@
+use_frameworks!
+target 'ReativeMusic'
+pod 'ReactiveCocoa'
---
+++ Pods/Target Support Files/Pods-ReativeMusic/Pods-ReativeMusic-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ReativeMusic : NSObject
+@end
+@implementation PodsDummy_Pods_ReativeMusic
+@end
