17073f67e851bab5c196f5a7313b663073bf0203
nelidaVelazquez <nelida.velazquez@dominos.com>
2017-01-28 19:29:30 -0500

    Add more UI elemenets and style buttons

--- ReativeMusic/PlayViewController.m
+++ ReativeMusic/PlayViewController.m
@@ -9,12 +9,22 @@
 #import "PlayViewController.h"
 #import "TrackViewModel.h"
 #import <WebImage/UIImageView+WebCache.h>
+#import <AVFoundation/AVFoundation.h>
+#import <SpotifyAudioPlayback/SpotifyAudioPlayback.h>
+#import <SpotifyMetadata/SpotifyMetadata.h>
+
+static NSString * const kPauseImageName = @"pause_button";
+static NSString * const kPlayImageName = @"play_button";
 
 @interface PlayViewController ()
 
 @property (strong, nonatomic) IBOutlet UILabel *trackTitle;
 @property (strong, nonatomic) IBOutlet UILabel *artistName;
 @property (strong, nonatomic) IBOutlet UIImageView *albumImage;
+
+@property (strong, nonatomic) IBOutlet UIButton *playButton;
+@property (strong, nonatomic) IBOutlet UISlider *progessBar;
+
 
 @end
 
@@ -32,8 +42,13 @@
 - (void)setupViews {
     self.trackTitle.text = self.trackViewModel.tracKName;
     self.artistName.text = self.trackViewModel.artistName;
+    [self.playButton setImage:[UIImage imageNamed:kPlayImageName] forState:UIControlStateSelected];
+    [self.playButton setImage:[UIImage imageNamed:kPauseImageName] forState:UIControlStateNormal];
     [self.albumImage sd_setImageWithURL:self.trackViewModel.albumImageURL];
 }
 
+- (IBAction)pausePlayButton:(UIButton *)button {
+    button.selected = !button.selected;
+}
 
 @end
