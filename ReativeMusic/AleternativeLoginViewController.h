221aa5ff6e3a69904841f438c66bc716d4ab9d6e
nelidaVelazquez <nelida.velazquez@dominos.com>
2017-01-29 19:54:09 -0500

    Creating and alternative login

---
+++ ReativeMusic/AleternativeLoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  AleternativeLoginViewController.h
+//  ReativeMusic
+//
+//  Created by Nelida Velazquez on 1/29/17.
+//  Copyright © 2017 tolkiana. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AleternativeLoginViewController : UIViewController
+
+@end
---
+++ ReativeMusic/AleternativeLoginViewController.m
@@ -0,0 +1,22 @@
+//
+//  AleternativeLoginViewController.m
+//  ReativeMusic
+//
+//  Created by Nelida Velazquez on 1/29/17.
+//  Copyright © 2017 tolkiana. All rights reserved.
+//
+
+#import "AleternativeLoginViewController.h"
+
+@interface AleternativeLoginViewController ()
+
+@property (strong, nonatomic) IBOutlet UITextField *userTextField;
+@property (strong, nonatomic) IBOutlet UITextField *passwordTextField;
+@property (strong, nonatomic) IBOutlet UIButton *signInButton;
+@property (strong, nonatomic) IBOutlet UILabel *failureLabel;
+
+@end
+
+@implementation AleternativeLoginViewController
+
+@end
